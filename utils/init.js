f2c7abd78561fb880c4e3c159d979a4ec7a50917
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-09-16 18:35:58 +0600

    App Updated

--- utils/init.js
+++ utils/init.js
@@ -14,6 +14,7 @@ const init = ()=> {
                 key: SITE_PROPERTIES,
                 catagories: [],
                 sizes: [],
+                productTypes: [],
                 defaultTheme: 'light'
             })
         }
